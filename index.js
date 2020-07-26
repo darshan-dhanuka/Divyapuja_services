4e6f3b4808dbe10a52395102b29e27618de52724
Darshan <darshan.dhanukaa@gmail.com>
2020-07-26 23:37:39 +0530

    pandit

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const  bodyParser  =  require('body-parser');
 const  sqlite3  =  require('sqlite3').verbose();
 const  mysql = require('mysql');
 const  jwt  =  require('jsonwebtoken');
-const  bcrypt  =  require('bcryptjs'); 
-const cors = require('cors')
+const  bcrypt  =  require('bcryptjs');
+const cors = require('cors');
+var multer = require('multer');
+var fs = require("fs");
+
+var upload = multer({ dest: '/tmp' })
 
 const  app  =  express();
 const  router  =  express.Router();
@@ -27,10 +31,28 @@ connection.connect((err) => {
 const  createUsersTable  = () => {
     const  sqlQuery  =  `
         CREATE TABLE IF NOT EXISTS users (
-        id int AUTO_INCREMENT PRIMARY KEY, 
+        id int AUTO_INCREMENT PRIMARY KEY,
         name text,
         email varchar(150) UNIQUE,
         password text)`;
+
+    return  connection.query(sqlQuery, function(err, results, fields) {
+                if (err) {
+                console.log(err.message);
+                }
+            });
+}
+
+
+
+const  createPanditTable  = () => {
+    const  sqlQuery  =  `
+        CREATE TABLE IF NOT EXISTS tbl_pandit_info (
+        id int AUTO_INCREMENT PRIMARY KEY,
+        pandit_name varchar(150) ,email_id varchar(250) ,mobile_num varchar(20) ,address text ,
+        city varchar(150) ,dob varchar(150) ,languages_known varchar(250) ,experience varchar(20)
+        ,adhar_num varchar(30) , pathshala_name  varchar(250) ,pandit_cat  varchar(250) ,about  text,
+        photo_url varchar(250) )`;
 
     return  connection.query(sqlQuery, function(err, results, fields) {
                 if (err) {
@@ -44,6 +66,11 @@ const  findUserByEmail  = (email, cb) => {
             cb(err, row)
     });
 }
+const  findPanditCat  = (cb) => {
+    return  connection.query(`SELECT * FROM tbl_pandit_category `, (err, row) => {
+            cb(err, row)
+    });
+}
 
 const  createUser  = (user, cb) => {
     let sql = 'INSERT INTO users (name, email, password) VALUES ("'+user[0]+'","'+user[1]+'","'+user[2]+'")';
@@ -51,14 +78,22 @@ const  createUser  = (user, cb) => {
         cb(err, row)
     });
 }
+const  createPandit  = (user, cb) => {
+    let sql = 'INSERT INTO tbl_pandit_info (pandit_name, email_id, mobile_num ,address ,city,dob ,languages_known ,experience,adhar_num , pathshala_name, pandit_cat, about ,photo_url  ) VALUES ("'+user["body"]["name"]+'","'+user["body"]["email_id"]+'","'+user["body"]["mobile_num"]+'",,"'+user["body"]["address"]+'" ,,"'+user["body"]["city"]+'","'+user["body"]["dob"]+'","'+user["body"]["languages_known"]+'","'+user["body"]["experience"]+'","'+user["body"]["adhar_num"]+'" ,"'+user["body"]["pathshala_name"]+'" ,"'+user["body"]["pandit_cat"]+'" ,"'+user["body"]["about"]+'" ,"'+user["body"]["photo_url"]+'")';
+    connection.query(sql,(err, row) => {
+        cb(err, row)
+    });
+}
 
 createUsersTable();
+createPanditTable();
 router.use(bodyParser.urlencoded({ extended:  false }));
 router.use(bodyParser.json());
 
 router.post('/register', (req, res) => {
-    const  name  =  req.body.name;
-    const  email  =  req.body.email;
+console.log(req);
+    const  name  =  req.body.firstName;
+    const  email  =  req.body.username;
     const  password  =  bcrypt.hashSync(req.body.password);
 
     createUser([name, email, password], (err)=>{
@@ -76,10 +111,11 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-    const  email  =  req.body.email;
+console.log(req);
+    const  email  =  req.body.username;
     const  password  =  req.body.password;
     findUserByEmail(email, (err, user)=>{
-        
+       
         var resultArray = Object.values(JSON.parse(JSON.stringify(user)))
         if (err) return  res.status(500).send('Server error!');
         if (!resultArray[0]) return  res.status(404).send('User not found!');
@@ -94,6 +130,46 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.get('/users', (req, res) => {
+    res.status(200).send('This is an authentication server');
+});
+router.get('/pandit_cat', (req, res) => {
+    findPanditCat((err,result)  => {
+        var resultArray = Object.values(JSON.parse(JSON.stringify(result)));
+        console.log(resultArray);
+        if (err) return  res.status(500).send('Server error!');
+        if (!resultArray[0]) return  res.status(404).send('No data found!');
+        res.status(200).send({ "data":  resultArray});
+    });
+});
+router.post('/pandit_register', (req, res) => {
+    createPandit(req, (err)=>{
+        if(err) return  res.status(500).send("Server error!");
+        if (err) return  res.status(500).send('Server error!');  
+        res.status(200).send({ "status":  "Registered Successfully" });
+    });
+});
+router.post('/file_upload', upload.single("file"), function (req, res) {
+    console/log(file);
+    var file = __dirname + "/" + req.file.originalname;
+    fs.readFile( req.file.path, function (err, data) {
+         fs.writeFile(file, data, function (err) {
+          if( err ){
+               console.error( err );
+               response = {
+                    message: 'Sorry, file couldn\'t be uploaded.',
+                    filename: req.file.originalname
+               };
+          }else{
+                response = {
+                    message: 'File uploaded successfully',
+                    filename: req.file.originalname
+               };
+           }
+           res.end( JSON.stringify( response ) );
+        });
+    });
+ })
 router.get('/', (req, res) => {
     res.status(200).send('This is an authentication server');
 });
