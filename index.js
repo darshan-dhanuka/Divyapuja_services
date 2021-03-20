58ff0f946cedf365ef9df20eb04158127ea8b4e3
darshan-psl <darshan.dhanuka@pokersportsleague.com>
2021-03-20 18:05:04 +0530

    moonsignapi

--- index.js
+++ index.js
@@ -322,7 +322,7 @@ router.post('/file_upload', upload.single("file"), function (req, res) {
  })
 
  router.get('/get_moonsign',  function (req, res) {
-    console.log(req);
+    res.status(200).send({ "status":  "Success", "data":req });
  })
 router.get('/', (req, res) => {
     res.status(200).send('This is an authentication server');
