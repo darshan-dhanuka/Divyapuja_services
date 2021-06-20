c82ad9dd151e1762372e9baa0145097727d371b6
rupeshgardi04 <phpdeveloper0104@gmail.com>
2021-06-20 20:14:35 +0530

    get single product

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -2,14 +2,14 @@ const DbInstance = require('../DbConnection');
 const sequelize = DbInstance.getSequelizeInstance();
 const Op = DbInstance.Sequelize.Op;
 
-module.exports.getProducts = (user_id) => {
+module.exports.getProducts = (prod_id) => {
   let whrCondition = '';
-//   if (typeof user_id !== 'undefined') {
-//     whrCondition = `where user_id = ${user_id}`;
-//   }
+  if (typeof prod_id !== 'undefined') {
+    whrCondition = ` where p.id = ${prod_id}`;
+  }
 
   let query = `SELECT p.*, c.name as category_name FROM  tbl_products p LEFT JOIN tbl_product_category c ON p.cat_id = c.id ${whrCondition} AND p.is_deleted = "0"`
-  
+  console.log('query:: ' + query)
   return sequelize.query(query, {
     type: 'SELECT'
   }).then(async result => {
